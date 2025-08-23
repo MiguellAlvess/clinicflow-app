02bc1e618c141e1f76ad17b2e450985ce315d3e8
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-08-23 11:09:05 -0300

    feat: add schema validation for creating appointments

---
+++ src/actions/create-appointment/schema.ts
@@ -0,0 +1,10 @@
+import { z } from "zod"
+
+export const createAppointmentSchema = z.object({
+  patientId: z.string().min(1, { message: "O paciente é obrigatório" }),
+  doctorId: z.string().min(1, { message: "O médico é obrigatório" }),
+  appointmentPrice: z.number().min(1, {
+    message: "O valor da consulta é obrigatório",
+  }),
+  date: z.date({ message: "A data é obrigatória" }),
+})
