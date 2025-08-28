b23928c2ef34f36fe6c02e47598d4b28886345ac
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-08-28 19:24:49 -0300

    feat: redirect to dashboard on home page load

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,7 @@
-import { Button } from "@/components/ui/button"
+import { redirect } from "next/navigation"
 
 const Home = () => {
-  return (
-    <div className="flex">
-      <div className="mb-4 text-4xl font-bold text-blue-600">ClinicFlow</div>
-      <div className="mb-4 text-4xl font-bold">
-        Bem-vindo ao sistema de gestão clínica
-      </div>
-      <Button>Entrar</Button>
-    </div>
-  )
+  redirect("/dashboard")
 }
 
 export default Home
